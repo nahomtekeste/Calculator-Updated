72f47fb1a1f4d63cdc25496e9fd29f19083275d7
nahomtekeste <46280184+nahomtekeste@users.noreply.github.com>
2019-07-06 12:40:19 +0100

    Add files via upload

---
+++ newsubclass.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cacultoion;
+
+/**
+ *
+ * @author user
+ */
+public class newsubclass extends caculator{
+ 
+    
+    
+    public void diposite(){
+        System.out.println("the impilimetation of the one");
+    }
+    
+    public static void main(String[]args){
+        
+            caculator myobjetc = new newsubclass();
+                myobjetc.setVisible(true);
+    }
+  
+}
